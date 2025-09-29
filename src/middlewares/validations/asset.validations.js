b5b170ebf328123e3c77a0a183db371e00b6e52f
Alejandro <BB_rae@hotmail.com>
2025-09-29 11:01:30 -0300

    Terminadas las validaciones de user, category y asset

--- src/middlewares/validations/asset.validations.js
+++ src/middlewares/validations/asset.validations.js
@@ -1,3 +1,52 @@
+import { body } from "express-validator";
+import { AssetModel } from "../../models/mongoose/asset.model.js";
+
 export const createAssetValidation = [
   // TODO: completar las validaciones para crear un recurso
+  body("inventory_number")
+    .notEmpty()
+    .withMessage("Faltan campos obligatorios")
+    .custom(async (value) => {
+      const asset = AssetModel.findOne({ inventoryNumber: value });
+      if (asset) {
+        throw new Error("Inventory_number registrado");
+      }
+    }),
+  body("description")
+    .notEmpty()
+    .withMessage("Faltan campos obligatorios")
+    .isLength({ min: 10, max: 500 })
+    .withMessage(
+      "El campobo debe contener entre 10 y 500 caracteres inclusive"
+    ),
+  body("brand")
+    .notEmpty()
+    .withMessage("Faltan campos obligatorios")
+    .isLength({ min: 2, max: 100 })
+    .withMessage("El campobo debe contener entre 2 y 100 caracteres inclusive"),
+  ,
+  body("model")
+    .notEmpty()
+    .withMessage("Faltan campos obligatorios")
+    .isLength({ min: 2, max: 100 })
+    .withMessage("El campobo debe contener entre 2 y 100 caracteres inclusive"),
+  ,
+  body("status")
+    .optional()
+    .notEmpty()
+    .withMessage("El campo no puede estar vacío")
+    .contains("good", "regular", "bad", "out_of_service")
+    .withMessage(
+      " el estatus solo puede ser, good, regular, bad o out_of_service"
+    ),
+  body("acquisitionDate")
+    .notEmpty()
+    .withMessage("Faltan campos obligatorios")
+    .isDate()
+    .withMessage("El campo solo acepta fecha como formato"),
+  body("acquisitionValue")
+    .notEmpty()
+    .withMessage("Faltan campos obligatorios")
+    .isNumeric()
+    .withMessage("El campo debe ser un número"),
 ];
--- src/middlewares/validations/category.validations.js
+++ src/middlewares/validations/category.validations.js
@@ -1,3 +1,23 @@
+import { body } from "express-validator";
+import { CategoryModel } from "../../models/mongoose/category.model.js";
+
 export const createCategoryValidation = [
   // TODO: completar las validaciones para crear una categoria
+  body("name")
+    .notEmpty()
+    .withMessage("Faltan campos obligatorios")
+    .isLength({ min: 3, max: 100 })
+    .withMessage("El campo debe contener entre 3 y 100 caracteres inclusive")
+    .custom(async (value) => {
+      const catagory = CategoryModel.findOne({ name: value });
+      if (catagory) {
+        throw new Error("Name registrado");
+      }
+    }),
+  body("description")
+    .optional()
+    .notEmpty()
+    .withMessage("El campo no puede estar vacío")
+    .isLength({ max: 500 })
+    .withMessage("El campo solo puede contener hasta 500 caracteres inclusive"),
 ];
